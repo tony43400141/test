97ce3a48729ac3b1dc76b00716ad01017179d933
tony43400141 <huamanlou@b5m.com>
2015-03-05 10:18:31 +0800

    ghhhhhhh

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+trait Hello
+{
+	public function sayHello()
+	{
+		echo 'hello';
+	}
+	
+}
+trait Word
+{
+	public function sayWord()
+	{
+		echo '  world';
+	}
+	
+}
+class HelloWord
+{
+	use Hello,Word;
+	public function showInfo()
+	{
+		echo '!';
+	}
+}
+
+$hw = new HelloWord();
+$hw->sayHello();
+$hw->sayWord();
+$hw->showInfo();
+?>
+
